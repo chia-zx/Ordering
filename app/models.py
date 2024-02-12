e96c38716dfbb3c4b3c48311ee2dd3eb2427a3e3
Fatma-48 <157384305+Fatma-48@users.noreply.github.com>
2024-02-12 13:00:57 +0800

    Update models.py
    added in order module

--- app/models.py
+++ app/models.py
@@ -98,6 +98,8 @@ class Order(models.Model):
     order_totalprice = models.DecimalField(max_digits=10, decimal_places=2)
     order_status = models.CharField(max_length=20, choices=STATUS)
     customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    vendor_id = models.ForeignKey(Vendor, on_delete=models.CASCADE, null=True)
+    delivery_status = models.CharField(max_length=20, choices=STATUS, default='Pending')
     deliveryperson_id = models.ForeignKey(DeliveryPerson, on_delete=models.CASCADE, null=True)
     def __str__(self):
         return str(self.order_id)

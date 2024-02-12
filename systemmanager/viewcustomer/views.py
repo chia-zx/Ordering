d9e1939586c87f31c90ebb784b1b6a920d2a3e4e
chia-zx <1211104495@student.mmu.edu.my>
2024-02-12 22:00:26 +0800

    Revert "Remove redundant code"
    This reverts commit 41ab39de3e161adf0910adb26cb47f66bf5dc994.

--- systemmanager/viewcustomer/views.py
+++ systemmanager/viewcustomer/views.py
@@ -1,30 +1,21 @@
+from datetime import datetime
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.db.models import Sum
-from app.models import Customer, Order, OrderItem;
+from app.models import Customer;
 
 @login_required
 def viewAllCustomer(request):
     sort_by = request.GET.get('customer')
 
     if sort_by == 'ID':
-        customers = Customer.objects.all().order_by('customer_id')
+        customers = Customer.objects.all().order_by('customer_id').values()
     elif sort_by == 'Name':
-        customers = Customer.objects.all().order_by('customer_name')
+        customers = Customer.objects.all().order_by('customer_name').values()
     elif sort_by == 'Spent':
-        customers = Customer.objects.all().order_by('customer_spent')
+        customers = Customer.objects.all().order_by('customer_spent').values()
     else:
-        customers = Customer.objects.all()
+        customers = Customer.objects.all().values()
         # ? display all (name, phone, address, spent)
-
-    for customer in customers:
-        orders = Order.objects.filter(customer_id=customer.customer_id, order_status='Completed')
-
-        total_spent = orders.aggregate(Sum('order_totalprice'))['order_totalprice__sum'] or 0
-        total_spent = "{:.2f}".format(total_spent)
-
-        customer.customer_spent = total_spent
-        customer.save()
 
     context = {
         'customers' : customers,
--- systemmanager/viewvendor/views.py
+++ systemmanager/viewvendor/views.py
@@ -1,33 +1,21 @@
+from datetime import datetime
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.db.models import Sum
-from app.models import Order, OrderItem, Vendor
+from app.models import Vendor;
 
 @login_required
 def viewAllVendor(request):
     sort_by = request.GET.get('vendor')
 
     if sort_by == 'ID':
-        vendors = Vendor.objects.all().order_by('vendor_id')
+        vendors = Vendor.objects.all().order_by('vendor_id').values()
     elif sort_by == 'Name':
-        vendors = Vendor.objects.all().order_by('vendor_name')
+        vendors = Vendor.objects.all().order_by('vendor_name').values()
     elif sort_by == 'Revenue':
-        vendors = Vendor.objects.all().order_by('vendor_revenue')
+        vendors = Vendor.objects.all().order_by('vendor_revenue').values()
     else:
-        vendors = Vendor.objects.all()
+        vendors = Vendor.objects.all().values()
         # ? display all (name, phone, address, revenue)
-
-    for vendor in vendors:
-        complete_orderid = OrderItem.objects.filter(
-                                food_id__vendor_id=vendor.vendor_id, 
-                                order_id__order_status='Completed').values_list('order_id', flat=True)
-        orders = Order.objects.filter(order_id__in=complete_orderid, order_status='Completed')
-
-        total_revenue = orders.aggregate(Sum('order_totalprice'))['order_totalprice__sum'] or 0
-        total_revenue = "{:.2f}".format(total_revenue)
-
-        vendor.vendor_revenue = total_revenue
-        vendor.save()
 
     context = {
         'vendors' : vendors,

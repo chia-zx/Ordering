4d572ffddd157a427aa50365de1dc8309d4096b4
ahmedelfatih1 <134754311+ahmedelfatih1@users.noreply.github.com>
2024-02-12 13:18:49 +0800

    round off total spent

--- customer/viewtotalspent/views.py
+++ customer/viewtotalspent/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 from app.models import Customer,Order
 from datetime import datetime
 from django.db.models import Sum
+from decimal import Decimal
 
 @login_required
 def customer_total_spent(request):
@@ -11,19 +12,19 @@ def customer_total_spent(request):
     customer = Customer.objects.get(user=request.user)
         
     # Get the total amount spent from the Customer instance
-    """customer_spent = customer.customer_spent""" 
-    total_spent = Order.objects.filter(
+    total_spent_result = Order.objects.filter(
         customer_id=customer, order_status='Completed'
-    ).aggregate(total_spent=Sum('order_totalprice'))['total_spent']
-
-
+    ).aggregate(total_spent=Sum('order_totalprice'))
+    
+    total_spent = total_spent_result['total_spent'] or 0
+    total_spent_rounded = round(total_spent, 2)  # Round to 2 decimal places
 
     # Fetch all completed orders for the customer
     orders = Order.objects.filter(customer_id=customer, order_status='Completed').order_by('-order_date')
 
 
     context = {
-        'customer_spent': total_spent,
+        'customer_spent': total_spent_rounded,
         'orders': orders,
         'year': datetime.now().year
     }
